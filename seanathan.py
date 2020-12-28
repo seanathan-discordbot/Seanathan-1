c57ce50a716062ddbcaca24978dcd2b63cdfcb51
seanrowland101 <54593108+seanrowland101@users.noreply.github.com>
2020-12-28 16:48:00 -0500

    Update seanathan.py

--- seanathan.py
+++ seanathan.py
@@ -1,19 +1,48 @@
 import discord
+from discord.ext import commands
+import os
+import asyncio
 
-TOKEN
-
-client = discord.Client()
-
+Token
+client = commands.Bot(command_prefix = '$')
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@client.command()
+async def hello(ctx):
+    await ctx.message.channel.send('Hello!')
+@client.command()
+async def sean(ctx):
+    await ctx.message.channel.send('Sean is short for Seanathan')
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+@client.command()
+async def delay(ctx):
+    await asyncio.sleep(60)
+    await ctx.message.channel.send('Delay')
 
-client.run(TOKEN)
+@client.command()
+async def echo(ctx, *, returnStatement):
+    await ctx.send(returnStatement)
+@client.command()
+async def is_num(ctx,*,returnStatement):
+    if returnStatement.isdigit()==False:
+        await ctx.send('Please enter a number')
+    else:
+        await ctx.send(returnStatement)
+        
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(ctx.author.mention)
+
+@client.command() #work on this tommorow 
+async def remind(ctx,*,returnStatement):
+    if returnStatement.isdigit()==False:
+        await ctx.send('Please enter a number')
+    else:
+        await ctx.send("I'll remind you in "+returnStatement)
+        pause=int(returnStatement)
+        await asyncio.sleep(pause)
+        await ctx.send(ctx.author.mention+" your reminder")
+   client.run(Token)
