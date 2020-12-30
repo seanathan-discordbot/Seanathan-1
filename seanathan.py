84a42092413494bb73fe92327eda3006bcfaee2d
seanrowland101 <54593108+seanrowland101@users.noreply.github.com>
2020-12-29 21:20:54 -0500

    Update seanathan.py

--- seanathan.py
+++ seanathan.py
@@ -1,51 +1,19 @@
-import discord
+import discord 
+import os 
 from discord.ext import commands
-import os
-import asyncio
-
-Token #add discord bot token here 
-client = commands.Bot(command_prefix = '$')
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+Token #the token of the bot
+client=commands.Bot(command_prefix="!")
 
 @client.command()
-async def hello(ctx):
-    await ctx.message.channel.send('Hello!')
-@client.command()
-async def sean(ctx):
-    await ctx.message.channel.send('Sean is short for Seanathan')
+async def load(ctx,extension):
+    client.load_extension(f'cogs.{extension}')
 
 @client.command()
-async def delay(ctx):
-    await asyncio.sleep(60)
-    await ctx.message.channel.send('Delay')
+async def unload(ctx,extension):
+    client.unload_extension(f'cogs.{extension}')
 
-@client.command()
-async def echo(ctx, *, returnStatement):
-    await ctx.send(returnStatement)
-@client.command()
-async def is_num(ctx,*,returnStatement):
-    if returnStatement.isdigit()==False:
-        await ctx.send('Please enter a number')
-    else:
-        await ctx.send(returnStatement)
-        
-
-@client.command()
-async def ping(ctx):
-    await ctx.send(ctx.author.mention)
- """
-incorporate into a cog called remind. 
-Also need to test how remind works with multiple reminders
-"""
-@client.command() 
-async def remind(ctx,*,returnStatement):
-    if returnStatement.isdigit()==False:
-        await ctx.send('Please enter a number')
-    else:
-        await ctx.send("I'll remind you in "+returnStatement)
-        pause=int(returnStatement)
-        await asyncio.sleep(pause)
-        await ctx.send(ctx.author.mention+" your reminder")
-   client.run(Token)
+# Runs at bot startup to load all cogs
+for filename in os.listdir(r'C:\Users\rowlas2\Documents\Seanathan\cogs'): # go into cog folder and search all files
+    if filename.endswith('.py'): # see if the file is a python file
+        client.load_extension(f'cogs.{filename[:-3]}') # cuts off last 3 chars
+client.run(Token)
