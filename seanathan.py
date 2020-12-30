e5221464d1feb10c99341add0185e502d8d1b4a0
seanrowland101 <54593108+seanrowland101@users.noreply.github.com>
2020-12-30 17:16:46 -0500

    Update seanathan.py

--- seanathan.py
+++ seanathan.py
@@ -5,15 +5,19 @@ Token #the token of the bot
 client=commands.Bot(command_prefix="!")
 
 @client.command()
+#allows owner to load a cog if they make changes while bot is online 
 async def load(ctx,extension):
+    await ctx.message.channel.send('loaded')
     client.load_extension(f'cogs.{extension}')
 
 @client.command()
+#allows user to unload cog if changes are made while bot is running
 async def unload(ctx,extension):
+    await.ctx.message.send('unloaded')
     client.unload_extension(f'cogs.{extension}')
 
 # Runs at bot startup to load all cogs
-for filename in os.listdir(r'C:\Users\rowlas2\Documents\Seanathan\cogs'): # go into cog folder and search all files
-    if filename.endswith('.py'): # see if the file is a python file
-        client.load_extension(f'cogs.{filename[:-3]}') # cuts off last 3 chars
+for filename in os.listdir(r'C:\Users\rowlas2\Documents\Seanathan\cogs'): #replace path with your own file
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}') 
 client.run(Token)
