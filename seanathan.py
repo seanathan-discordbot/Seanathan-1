85b4aed45dceabc32c6b72669c30ae9d66362292
seanrowland101 <54593108+seanrowland101@users.noreply.github.com>
2020-12-31 13:29:58 -0500

    Update seanathan.py

--- seanathan.py
+++ seanathan.py
@@ -1,23 +1,32 @@
-import discord 
-import os 
+import discord
+import os
 from discord.ext import commands
-Token #the token of the bot
+Token='NzY3NTQ2Njc4ODQ0NDU3MDMy.X4zfjA.sCv_ohI9n9MrXcOWq-dUQqCcPh8'
 client=commands.Bot(command_prefix="!")
-
+admins = [196465885148479489, 325080171591761921, 530760994289483790, 145339105239105537] #Aramy, Sean, Demi, Roberto
+def is_admin(ctx):
+        if ctx.author.id in admins:
+            return True
 @client.command()
-#allows owner to load a cog if they make changes while bot is online 
-async def load(ctx,extension):
-    await ctx.message.channel.send('loaded')
+@commands.check(is_admin)
+async def load(ctx, extension):
     client.load_extension(f'cogs.{extension}')
+    await ctx.channel.send('Loaded.')
 
 @client.command()
-#allows user to unload cog if changes are made while bot is running
-async def unload(ctx,extension):
-    await.ctx.message.send('unloaded')
+@commands.check(is_admin)
+async def unload(ctx, extension):
     client.unload_extension(f'cogs.{extension}')
+    await ctx.channel.send('Unloaded.')
+
+@client.command()
+@commands.check(is_admin)
+async def reload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+    client.load_extension(f'cogs.{extension}')
+    await ctx.channel.send('Reloaded.')
 
 # Runs at bot startup to load all cogs
-for filename in os.listdir(r'C:\Users\rowlas2\Documents\Seanathan\cogs'): #replace path with your own file
-    if filename.endswith('.py'):
-        client.load_extension(f'cogs.{filename[:-3]}') 
+for filename in os.listdir(r'C:\Users\rowlas2\Documents\Seanathan\cogs'):
+        client.load_extension(f'cogs.{filename[:-3]}')
 client.run(Token)
